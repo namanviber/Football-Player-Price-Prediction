53d4ca1b02b0ab7c303e306ed7f32475e237681e
Naman Jain <namanviber@gmail.com>
2023-05-08 23:31:55 +0530

    updates

--- app.py
+++ app.py
@@ -10,14 +10,14 @@ big_club_options = [0,1]
 
 Page_views = st.number_input('Enter Page Views')
 Fpl_Value = st.number_input('Enter Fpl_Value')
-Fpl_Selection = st.number_input('Enter Fpl_Selection')
+Fpl_Selection = st.slider(min_value=0.01, max_value= 100.00, label="Fpl_Selection Percentage")
 Fpl_Points = st.number_input('Enter Fpl_Points')
 Big_Club = st.selectbox(options= big_club_options, label= "Big Club (Yes --> 1 , No --> 2 )")
 
 
 def predict():
     x = pd.DataFrame([])
-    x[["page_views",	"fpl_value",	"fpl_sel",	"fpl_points",	"big_club"]] = [[Page_views,Fpl_Value,Fpl_Selection,Fpl_Points,Big_Club]]
+    x[["page_views",	"fpl_value",	"fpl_sel",	"fpl_points",	"big_club"]] = [[Page_views,Fpl_Value,Fpl_Selection/100,Fpl_Points,Big_Club]]
     price = model.predict(x)
     label = price[0]
     print(type(label))
